651dfefdf999ced8626eb54d868fa477854c18d0
James Butts <butt1738@bears.unco.edu>
2019-11-04 18:07:42 -0700

    Updated participants.py

--- code/participants.py
+++ code/participants.py
@@ -5,17 +5,17 @@
 def main():
     numPeople = 13 # number of students + instructor
 
-    # Add your name to the list below to the right of any other names.  
+    # Add your name to the list below to the right of any other names.
     # I have added my name plus a placeholder for your name. Replace the placeholder with your name.
     # The last person to push the file to the repository should ensure the <your name here> placeholder is removed.
 
-    people = ["Christopher Harris", "Evan Minor", "Devin Panikkar", "Lauren Simms", "Fernando Espinoza", "Vandiver David", "Matthew Bradow","Mandeep Singh","Edgar Diaz",
+    people = ["Christopher Harris", "Evan Minor", "Devin Panikkar", "Lauren Simms", "Fernando Espinoza", "Vandiver David", "Matthew Bradow","Mandeep Singh","Edgar Diaz", "James Butts",
               "<your name here>"]
 
     # don't change anything below this line\n",
 
     for i in range(len(people)):
         print("{0:2d}. {1}".format(i+1, people[i]))
-              
+
     print("\nNumber of People: %2d" %(i+1))
     print("Percentage of People: {:.1%}".format((i+1)/numPeople))
